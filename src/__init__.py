e9ab8bc0903ec24f7be96b5e0f3f012bd8af17af
ijgnd <fooijgnd@barijgnd>
2020-02-22 13:34:19 +0100

    new file for 2.1.20+

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,8 @@
-from . import change_notetype_deck
+from anki import version as anki_version
+
+old_anki = tuple(int(i) for i in anki_version.split(".")) < (2, 1, 20)
+
+if old_anki:
+    from . import old_change_notetype_deck
+else:
+    from . import new_change_notetype_deck
